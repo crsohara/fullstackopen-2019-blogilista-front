1076fe0151a7954981a8486644e3920b6a8845f0
Mikko Saari <mikko@mikkosaari.fi>
2019-04-27 14:11:09 +0300

    Osa 5, tehtävä 5.1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,9 +15,46 @@ const App = () => {
     )  
   }, [])
 
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value)
+  }
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value)
+  }
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+			const user = await loginService.login({
+				username, password,
+			})
+
+			blogService.setToken(user.token)
+			setUser(user)
+			setUsername('')
+			setPassword('')
+		} catch (exception) {
+		}
+  }
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Kirjaudu sisään</h2>
+        <form onSubmit={handleLogin}>
+          Käyttäjätunnus <input type="text" value={username} name="username" onChange={handleUsernameChange} /><br />
+          Salasana <input type="password" value={password} name="password" onChange={handlePasswordChange} /><br />
+          <button type="submit">Kirjaudu</button>
+        </form>
+      </div>
+    )
+  }
+
   return (
     <div>
-      <h2>blogs</h2>
+      <h2>Blogit</h2>
+      <p>Käyttäjätunnus: {user.name}.</p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
