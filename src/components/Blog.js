f1a2291eae8fe90f013d72a6ec45048dad4451e2
Mikko Saari <mikko@mikkosaari.fi>
2019-04-29 09:58:42 +0300

    Osa 5, tehtävä 5.6*

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,28 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [moreVisible, setMoreVisible] = useState(null)
+
+  const blogStyle = {
+    padding: 10,
+    border: 'thin solid black',
+    marginTop: 10,
+  }
+
+  const toggleMoreVisibility = () => {
+    setMoreVisible(!moreVisible)
+  }
+  const moreVisibilityStyle = {
+    display: moreVisible ? '' : 'none',
+    paddingTop: 10,
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div onClick={toggleMoreVisibility}>{blog.title} {blog.author}</div>
+      <div style={moreVisibilityStyle}>{blog.url}<br />
+      {blog.likes} tykkäystä <button>Tykkää</button><br />
+      Lisäsi {blog.user.username}</div>
+    </div>
+)}
 
 export default Blog
