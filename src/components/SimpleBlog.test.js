af2718c3b2992df42732204b0f35ed00ba67a181
Mikko Saari <mikko@mikkosaari.fi>
2019-04-30 09:13:29 +0300

    Osa 5, tehtävä 5.14

--- src/components/SimpleBlog.test.js
+++ src/components/SimpleBlog.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from 'react-testing-library'
+import { render, fireEvent } from 'react-testing-library'
 import SimpleBlog from './SimpleBlog'
 
 describe('<SimpleBlog />', () => {
@@ -20,4 +20,25 @@ describe('<SimpleBlog />', () => {
 		const likes = component.container.querySelector('.likes')
 		expect(likes).toHaveTextContent(/^10$/)
 	})
+
+	test('Tykkäysnapin painaminen kaksi kertaa tekee kaksi event handler -kutsua', async () => {
+		const blog = {
+			title: 'Blogin otsikko',
+			author: 'Blogin tekijä',
+			likes: 10,
+		}
+
+		const mockHandler = jest.fn()
+
+		const { getByText } = render(
+			<SimpleBlog blog={blog} onClick={mockHandler}/>
+		)
+
+		const button = getByText('like')
+		fireEvent.click(button)
+		fireEvent.click(button)
+
+		expect(mockHandler.mock.calls.length).toBe(2)
+	})
+
 })
