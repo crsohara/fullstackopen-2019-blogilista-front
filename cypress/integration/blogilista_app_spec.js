033d5e7731c80738e7eeac0e1668d008211e1b4c
Mikko Saari <mikko@mikkosaari.fi>
2019-09-25 10:19:55 +0300

    Tehtävä 7.18

--- cypress/integration/blogilista_app_spec.js
+++ cypress/integration/blogilista_app_spec.js
@@ -1,5 +1,12 @@
 describe("Blogilista ", function() {
 	beforeEach(function() {
+		cy.request("POST", "http://localhost:3000/api/tests/reset")
+		const user = {
+			name: "Mikko Saari",
+			username: "admin",
+			password: "salasana"
+		}
+		cy.request("POST", "http://localhost:3000/api/users/", user)
 		cy.visit("http://localhost:3000")
 	})
 
@@ -17,8 +24,20 @@ describe("Blogilista ", function() {
 		cy.contains("Tallenna uusi blogi")
 	})
 
-	it("blogin avaaminen onnistuu", function() {
-		cy.contains("Mikon suosikkiblogi").click()
+	it("blogin luominen onnistuu", function() {
+		cy.contains("Kirjaudu sisään").click()
+		cy.get("input[name=username]").type("admin")
+		cy.get("input[name=password]").type("salasana")
+		cy.get("[data-cy=login]").click()
+		cy.contains("Tallenna uusi blogi").click()
+		cy.get("input[name=title]").type("Mikon suosikkiblogi")
+		cy.get("input[name=author]").type("Mikko Makkonen")
+		cy.get("input[name=blogURL]").type("http://www.mikkomakkonen.fi/")
+		cy.get("input[type=submit]").click()
+		cy.reload()
+		cy.get("#blogs")
+			.contains("Mikon suosikkiblogi")
+			.click()
 		cy.contains("Lisäsi admin")
 	})
 })
